f2c011907cb89e0883ea53d7c4b12df9046e1583
Elias_Ahmed <eliasahmed16052002@gmail.com>
2025-06-09 21:43:16 +0600

    add agent Application page

---
+++ src/pages/AgentApplicationPage.jsx
@@ -0,0 +1,20 @@
+import AgentApplication from "../components/Applications/AgentApplication";
+import useApplicationAuth from "../hooks/useApplicationAuth";
+
+export default function AgentApplicationPage() {
+	const { loading, isAuthenticated } = useApplicationAuth();
+
+	if (loading) {
+		return (
+			<div className="min-h-screen bg-gray-50 flex items-center justify-center">
+				<div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary-600"></div>
+			</div>
+		);
+	}
+
+	if (!isAuthenticated) {
+		return null; // Will redirect to login
+	}
+
+	return <AgentApplication />;
+}
