ab72e9d3daf4a123b8cb028e09d8fc8e2d63c018
Elias_Ahmed <eliasahmed16052002@gmail.com>
2025-06-09 21:48:59 +0600

    add Seller Application  page

---
+++ src/pages/SellerApplicationPage.jsx
@@ -0,0 +1,20 @@
+import SellerApplication from "../components/Applications/SellerApplication";
+import useApplicationAuth from "../hooks/useApplicationAuth";
+
+export default function SellerApplicationPage() {
+	const { loading, isAuthenticated } = useApplicationAuth();
+
+	if (loading) {
+		return (
+			<div className="min-h-screen bg-gray-50 flex items-center justify-center">
+				<div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary-600"></div>
+			</div>
+		);
+	}
+
+	if (!isAuthenticated) {
+		return null; // Will redirect to login
+	}
+
+	return <SellerApplication />;
+}
