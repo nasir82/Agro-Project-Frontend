321a5309538c043bd9e5fb0ab7da6fc845b24014
Elias_Ahmed <eliasahmed16052002@gmail.com>
2025-06-10 12:23:29 +0600

    add index

---
+++ src/components/Deshboard/pages/admin/components/index.js
@@ -0,0 +1,16 @@
+// Badge components
+export * from "./Badges";
+
+// Stats components
+export { ProductStatsCards, UserStatsCards } from "./StatsCards";
+
+// Filter components
+export { ProductFiltersPanel, UserFiltersPanel } from "./FiltersPanel";
+
+// Data table components
+export { DataTable, Pagination } from "./DataTable";
+
+// Modal components
+export * from "./ModernModal";
+export { ProductModal } from "./ProductModal";
+export { UserModal } from "./UserModal";
